eeb6a11b45aace3b69dc21c2e365010a0f839c2e
Ayutthaya <nathanael.foy@gmail.com>
2012-08-10 15:06:03 +0200

    add nc_epoll.c nc_kqueue.c and  remove nc_event.c

---
+++ src/nc_epoll.c
@@ -0,0 +1,276 @@
+/*
+ * twemproxy - A fast and lightweight proxy for memcached protocol.
+ * Copyright (C) 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unistd.h>
+#include <nc_core.h>
+#include <nc_event.h>
+
+#ifdef NC_HAVE_EPOLL
+#include <sys/epoll.h>
+
+struct evbase * 
+evbase_create(int nevent, void (*callback_fp)(void *, uint32_t))
+{
+
+    struct evbase *evb;
+    int status, ep;
+    struct epoll_event *event;
+
+    if (nevent <= 0) {
+        log_error("nevent has to be positive %d", nevent);
+        return NULL;
+    }
+
+    ep = epoll_create(nevent);
+    if (ep < 0) {
+        log_error("epoll create of size %d failed: %s", nevent, strerror(errno));
+        return NULL;
+    }
+
+    event = nc_calloc(nevent, sizeof(*event));
+    if (event == NULL) {
+        status = close(ep);
+        if (status < 0) {
+            log_error("close e %d failed, ignored: %s", ep, strerror(errno));
+        }
+        return NULL;
+    }
+
+    evb = nc_alloc(sizeof(*evb));
+    if (evb == NULL) {
+        nc_free(event);
+        status = close(ep);
+        if (status < 0) {
+            log_error("close e %d failed, ignored: %s", ep, strerror(errno));
+        }
+        return NULL;
+
+    }
+
+    evb->nevent = nevent;
+    evb->ep = ep;
+    evb->event = event;
+    evb->callback_fp = callback_fp;
+
+    log_debug(LOG_INFO, "e %d with nevent %d", evb->ep,
+              evb->nevent);
+
+    return evb;
+}
+
+void
+evbase_destroy(struct evbase *evb)
+{
+    int status;
+
+    ASSERT(evb->ep >= 0);
+
+    nc_free(evb->event);
+
+    status = close(evb->ep);
+    if (status < 0) {
+        log_error("close e %d failed, ignored: %s", evb->ep, strerror(errno));
+    }
+    nc_free(evb);
+}
+
+int
+event_add_out(struct evbase *evb, struct conn *c)
+{
+    int status;
+    struct epoll_event event;
+    int ep = evb->ep;
+
+    ASSERT(ep > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(c->recv_active);
+
+    if (c->send_active) {
+        return 0;
+    }
+
+    event.events = (uint32_t)(EPOLLIN | EPOLLOUT | EPOLLET);
+    event.data.ptr = c;
+
+    status = epoll_ctl(ep, EPOLL_CTL_MOD, c->sd, &event);
+    if (status < 0) {
+        log_error("epoll ctl on e %d sd %d failed: %s", ep, c->sd,
+                  strerror(errno));
+    } else {
+        c->send_active = 1;
+    }
+
+    return status;
+}
+
+int
+event_del_out(struct evbase *evb, struct conn *c)
+{
+    int status;
+    struct epoll_event event;
+    int ep = evb->ep;
+
+    ASSERT(ep > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(c->recv_active);
+
+    if (!c->send_active) {
+        return 0;
+    }
+
+    event.events = (uint32_t)(EPOLLIN | EPOLLET);
+    event.data.ptr = c;
+
+    status = epoll_ctl(ep, EPOLL_CTL_MOD, c->sd, &event);
+    if (status < 0) {
+        log_error("epoll ctl on e %d sd %d failed: %s", ep, c->sd,
+                  strerror(errno));
+    } else {
+        c->send_active = 0;
+    }
+
+    return status;
+}
+
+int
+event_add_conn(struct evbase *evb, struct conn *c)
+{
+    int status;
+    struct epoll_event event;
+    int ep = evb->ep;
+
+    ASSERT(ep > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+
+    event.events = (uint32_t)(EPOLLIN | EPOLLOUT | EPOLLET);
+    event.data.ptr = c;
+
+    status = epoll_ctl(ep, EPOLL_CTL_ADD, c->sd, &event);
+    if (status < 0) {
+        log_error("epoll ctl on e %d sd %d failed: %s", ep, c->sd,
+                  strerror(errno));
+    } else {
+        c->send_active = 1;
+        c->recv_active = 1;
+    }
+
+    return status;
+}
+
+int
+event_del_conn(struct evbase *evb, struct conn *c)
+{
+    int status;
+    int ep = evb->ep;
+
+    ASSERT(ep > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+
+    status = epoll_ctl(ep, EPOLL_CTL_DEL, c->sd, NULL);
+    if (status < 0) {
+        log_error("epoll ctl on e %d sd %d failed: %s", ep, c->sd,
+                  strerror(errno));
+    } else {
+        c->recv_active = 0;
+        c->send_active = 0;
+    }
+
+    return status;
+}
+
+int
+event_wait(struct evbase *evb, int timeout)
+{
+    int nsd, i;
+    uint32_t evflags = 0;
+    int ep = evb->ep;
+    struct epoll_event *event = evb->event;
+    int nevent = evb->nevent;
+    void (*callback_fp)(void *, uint32_t) = evb->callback_fp;
+
+    ASSERT(ep > 0);
+    ASSERT(event != NULL);
+    ASSERT(nevent > 0);
+
+    for (;;) {
+        nsd = epoll_wait(ep, event, nevent, timeout);
+        if (nsd > 0) {
+            for (i = 0; i < nsd; i++) {
+                struct epoll_event *ev = &evb->event[i];
+
+                evflags = 0;
+                if (ev->events & EPOLLERR)
+                    evflags |= EV_ERR;
+
+                if (ev->events & EPOLLIN)
+                    evflags |= EV_READ;
+
+                if (ev->events & EPOLLOUT)
+                    evflags |= EV_WRITE;
+
+                if (callback_fp != NULL)
+                    (*callback_fp)((void *) ev->data.ptr, evflags);
+            }
+            return nsd;
+        }
+
+        if (nsd == 0) {
+            if (timeout == -1) {
+               log_error("epoll wait on e %d with %d events and %d timeout "
+                         "returned no events", ep, nevent, timeout);
+                return -1;
+            }
+
+            return 0;
+        }
+
+        if (errno == EINTR) {
+            continue;
+        }
+
+        log_error("epoll wait on e %d with %d events failed: %s", ep, nevent,
+                  strerror(errno));
+
+        return -1;
+    }
+    NOT_REACHED();
+}
+
+int
+event_add_st(struct evbase *evb, int fd)
+{
+    int status;
+    struct epoll_event ev;
+
+    ev.data.fd = fd;
+    ev.events = EPOLLIN;
+
+    status = epoll_ctl(evb->ep, EPOLL_CTL_ADD, fd, &ev);
+    if (status < 0) {
+        log_error("epoll ctl on e %d sd %d failed: %s", evb->ep, fd,
+                  strerror(errno));
+        return status;
+    }
+    
+    return status;
+}
+
+#endif /* NC_HAVE_EPOLL */
---
+++ src/nc_kqueue.c
@@ -0,0 +1,310 @@
+/*
+ * twemproxy - A fast and lightweight proxy for memcached protocol.
+ * Copyright (C) 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <unistd.h>
+#include <nc_core.h>
+#include <nc_event.h>
+
+#ifdef NC_HAVE_KQUEUE
+#include <sys/event.h>
+
+struct evbase *
+evbase_create(int nevent, void (*callback_fp)(void *, uint32_t))
+{
+
+    struct evbase *evb;
+    int status, kq;
+    struct kevent *changes, *kevents;
+
+    if (nevent <= 0) {
+        log_error("nevent has to be positive %d", nevent);
+        return NULL;
+    }
+
+    /* Initialize the kernel queue */
+    if ((kq = kqueue()) == -1) {
+        log_error("kernel queue create failed: %s", kq, strerror(errno));
+        return NULL;
+    }
+
+    changes = nc_calloc(nevent, sizeof(*changes));
+    if (changes == NULL) {
+        status = close(kq);
+        if (status < 0) {
+            log_error("close kq %d failed, ignored: %s", kq, strerror(errno));
+        }
+        return NULL;
+    }
+
+    kevents = nc_calloc(nevent, sizeof(*kevents));
+    if (kevents == NULL) {
+        nc_free(changes);
+        status = close(kq);
+        if (status < 0) {
+            log_error("close kq %d failed, ignored: %s", kq, strerror(errno));
+        }
+        return NULL;
+    }
+
+    evb = (struct evbase *) nc_alloc(sizeof(*evb));
+    if (evb == NULL) {
+        nc_free(changes);
+        nc_free(kevents);
+        status = close(kq);
+        if (status < 0) {
+            log_error("close kq %d failed, ignored: %s", kq, strerror(errno));
+        }
+        return NULL;
+    }
+
+    evb->kq = kq;
+    evb->changes = changes;
+    evb->kevents  = kevents;
+    evb->nevent = nevent; 
+    evb->callback_fp = callback_fp;
+    evb->n_changes = 0;
+
+    log_debug(LOG_INFO, "kq %d with nevent %d", evb->kq,
+              evb->nevent);
+
+    return evb;
+}
+
+void
+evbase_destroy(struct evbase *evb)
+{
+    int status;
+
+    ASSERT(evb->kq >= 0);
+
+    nc_free(evb->changes);
+    nc_free(evb->kevents);
+
+    status = close(evb->kq);
+    if (status < 0) {
+        log_error("close kq %d failed, ignored: %s", evb->kq, strerror(errno));
+    }
+    nc_free(evb);
+}
+
+int
+event_add_out(struct evbase *evb, struct conn *c)
+{
+    struct kevent *event;
+    int kq = evb->kq;
+
+    ASSERT(kq > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(c->recv_active);
+    ASSERT(evb->n_changes < evb->nevent);
+
+    if (c->send_active) {
+        return 0;
+    }
+
+    event = &evb->changes[(evb->n_changes)++];
+    memset(event, 0, sizeof(*event));
+
+    event->ident = c->sd;
+    event->filter = EVFILT_WRITE;
+    event->flags = EV_ADD | EV_CLEAR;
+    event->udata = (void *) c;
+
+    c->send_active = 1;
+
+    return 0;
+}
+
+int
+event_del_out(struct evbase *evb, struct conn *c)
+{
+    struct kevent *event;
+    int kq = evb->kq;
+
+    ASSERT(kq > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(c->recv_active);
+    ASSERT(evb->n_changes < evb->nevent);
+
+    if (!c->send_active) {
+        return 0;
+    }
+    
+    event = &evb->changes[(evb->n_changes)++];
+    memset(event, 0, sizeof(*event));
+
+    event->ident = c->sd;
+    event->filter = EVFILT_WRITE;
+    event->flags = EV_DELETE;
+    event->udata = (void *) c;
+
+    c->send_active = 0;
+
+    return 0;
+}
+
+int
+event_add_conn(struct evbase *evb, struct conn *c)
+{
+    struct kevent *event;
+    int kq = evb->kq;
+
+    ASSERT(kq > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(evb->n_changes < evb->nevent);
+
+    event = &evb->changes[(evb->n_changes)++];
+    memset(event, 0, sizeof(*event));
+
+    event->ident = c->sd;
+    event->filter = EVFILT_READ;
+    event->flags = EV_ADD | EV_CLEAR;
+    event->udata = (void *) c;
+
+    c->recv_active = 1;
+
+    event_add_out(evb, c);
+    c->send_active = 1;
+
+    return 0;
+}
+
+int
+event_del_conn(struct evbase *evb, struct conn *c)
+{
+    struct kevent *event;
+    int kq = evb->kq;
+
+    ASSERT(kq > 0);
+    ASSERT(c != NULL);
+    ASSERT(c->sd > 0);
+    ASSERT(evb->n_changes < evb->nevent);
+
+    event = &evb->changes[(evb->n_changes)++];
+    memset(event, 0, sizeof(*event));
+
+    event->ident = c->sd;
+    event->filter = EVFILT_READ;
+    event->flags = EV_DELETE;
+    event->udata = (void *) c;
+
+
+    event_del_out(evb, c);
+
+    c->recv_active = 0;
+    c->send_active = 0;
+
+    return 0;
+}
+
+int
+event_wait(struct evbase *evb, int timeout)
+{
+    int nsd, i;
+    uint32_t evflags = 0;
+    int kq = evb->kq;
+    int nevent = evb->nevent;
+    int n_changes = evb->n_changes;
+    struct timespec ts = nc_millisec_to_timespec(timeout);
+    void (*callback_fp)(void *, uint32_t) = evb->callback_fp;
+
+    ASSERT(kq > 0);
+    ASSERT(nevent > 0);
+
+    evb->n_changes = 0;
+
+    for (;;) {
+        nsd = kevent(kq, evb->changes, n_changes, evb->kevents, nevent, &ts);
+        if (nsd > 0) {
+            for (i = 0; i < nsd; i++) {
+                struct kevent *ev = &evb->kevents[i];
+
+                evflags = 0;
+                if (ev->flags == EV_ERROR) {
+                   /*
+                    * Error messages that can happen, when a delete fails.
+                    *   EBADF happens when the file descriptor has been
+                    *   closed,
+                    *   ENOENT when the file descriptor was closed and       
+                    *   then reopened.
+                    *   EINVAL for some reasons not understood; EINVAL
+                    *   should not be returned ever; but FreeBSD does :-\
+                    * An error is also indicated when a callback deletes
+                    * an event we are still processing.  In that case
+                    * the data field is set to ENOENT.
+                    */
+                    if (ev->data == EBADF ||
+                        ev->data == EINVAL ||
+                        ev->data == ENOENT)
+                        continue;
+                    evflags |= EV_ERR;
+                }
+
+                if (ev->filter == EVFILT_READ)
+                    evflags |= EV_READ;
+
+                if (ev->filter == EVFILT_WRITE)
+                    evflags |= EV_WRITE;
+
+                if (callback_fp != NULL)
+                    (*callback_fp)((void *)(ev->udata), evflags);
+            }
+            return nsd;
+        }
+
+        if (nsd == 0) {
+            if (timeout == -1) {
+               log_error("kqueue on kq %d with %d events and %d timeout "
+                         "returned no events", kq, nevent, timeout);
+                return -1;
+            }
+
+            return 0;
+        }
+
+        if (errno == EINTR) {
+            continue;
+        }
+
+        log_error("kevent on kq %d with %d events failed: %s", kq, nevent,
+                  strerror(errno));
+
+        return -1;
+    }
+    NOT_REACHED();
+}
+
+int
+event_add_st(struct evbase *evb, int fd)
+{
+    struct kevent *ev = &evb->changes[(evb->n_changes)++]; 
+
+    memset(ev, 0, sizeof(*ev));
+
+    ev->ident = fd;
+    ev->filter = EVFILT_READ;
+    ev->flags = EV_ADD | EV_CLEAR;
+    ev->udata = NULL;
+
+    return 0;
+}
+
+#endif /* NC_HAVE_KQUEUE */

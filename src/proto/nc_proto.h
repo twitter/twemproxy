0fcb81afe94aab076a8d6a9f7a5a984f4a970da6
Manju Rajashekhar <manj@twitter.com>
2012-11-13 10:18:58 -0800

    refactor parsing code and create header file nc_proto.h

---
+++ src/proto/nc_proto.h
@@ -0,0 +1,90 @@
+/*
+ * twemproxy - A fast and lightweight proxy for memcached protocol.
+ * Copyright (C) 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _NC_PROTO_H_
+#define _NC_PROTO_H_
+
+#include <nc_core.h>
+
+#ifdef NC_LITTLE_ENDIAN
+
+#define str4cmp(m, c0, c1, c2, c3)                                          \
+    (*(uint32_t *) m == ((c3 << 24) | (c2 << 16) | (c1 << 8) | c0))
+
+#define str5cmp(m, c0, c1, c2, c3, c4)                                      \
+    (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
+
+#define str6cmp(m, c0, c1, c2, c3, c4, c5)                                  \
+    (str4cmp(m, c0, c1, c2, c3) &&                                          \
+        (((uint32_t *) m)[1] & 0xffff) == ((c5 << 8) | c4))
+
+#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6)                              \
+    (str6cmp(m, c0, c1, c2, c3, c4, c5) && (m[6] == c6))
+
+#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                          \
+    (str4cmp(m, c0, c1, c2, c3) &&                                          \
+        (((uint32_t *) m)[1] == ((c7 << 24) | (c6 << 16) | (c5 << 8) | c4)))
+
+#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                      \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
+
+#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)                 \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                          \
+        (((uint32_t *) m)[2] & 0xffff) == ((c9 << 8) | c8))
+
+#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)            \
+    (str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && (m[10] == c10))
+
+#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)       \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                          \
+        (((uint32_t *) m)[2] == ((c11 << 24) | (c10 << 16) | (c9 << 8) | c8)))
+
+#else
+
+#define str4cmp(m, c0, c1, c2, c3)                                          \
+    (m[0] == c0 && m[1] == c1 && m[2] == c2 && m[3] == c3)
+
+#define str5cmp(m, c0, c1, c2, c3, c4)                                      \
+    (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
+
+#define str6cmp(m, c0, c1, c2, c3, c4, c5)                                  \
+    (str5cmp(m, c0, c1, c2, c3, c4) && m[5] == c5)
+
+#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6)                              \
+    (str6cmp(m, c0, c1, c2, c3, c4, c5) && m[6] == c6)
+
+#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                          \
+    (str7cmp(m, c0, c1, c2, c3, c4, c5, c6) && m[7] == c7)
+
+#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                      \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
+
+#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)                 \
+    (str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8) && m[9] == c9)
+
+#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)            \
+    (str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && m[10] == c10)
+
+#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)       \
+    (str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) && m[11] == c11)
+
+#endif
+
+void memcache_parse_req(struct msg *r);
+void memcache_parse_rsp(struct msg *r);
+
+#endif

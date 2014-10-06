99e150733c524b1de2e96a425870e64d6fd70bb2
Yash <yash@lyft.com>
2014-10-06 11:33:55 -0700

    pulling in PR 177 - do a dns lookup upon reconnect

--- src/nc_conf.h
+++ src/nc_conf.h
@@ -65,6 +65,7 @@ struct conf_listen {
 struct conf_server {
     struct string   pname;      /* server: as "name:port:weight" */
     struct string   name;       /* name */
+    struct string   address;    /* address */
     int             port;       /* port */
     int             weight;     /* weight */
     struct sockinfo info;       /* connect socket info */

04ff6ce71d8a0995f71e08b0ac8ef681dfac24f9
lwalkin <lwalkin@machinezone.com>
2015-02-04 20:04:05 -0800

    explore different variations of config parameters

--- tests/check_config_reload.py
+++ tests/check_config_reload.py
@@ -12,100 +12,129 @@ except:
     print("This program is designed to run `make check` to test.")
     exit(1)
 
+def test_code_reload(cfg_yml_params):
+
+    print "Testing code reload with parameters\n  %s" % cfg_yml_params
+
+    """
+    Open two server side sockets.
+    One is is for configuration A, another is for configuration B.
+    We'll create a nutcracker configuration and make it reload from A to B.
+    """
+
+    (listen_A, srv_A_port) = open_server_socket()
+    (listen_B, srv_B_port) = open_server_socket()
+
+    print("Opened two servers on ports %d and %d" % (srv_A_port, srv_B_port))
+
+    """
+    Open the third server on a port which we'll later assign to be a proxy port
+    for nutcracker. This is a little bit dangerous due to potential race
+    condition with other servers on the local system, but we can't do much
+    better than that within the time and space constraints provided for testing.
+    Just don't run high load servers or clients on this development machine
+    which does integration testing.
+    """
+
+    (proxy, proxy_port) = open_server_socket()
+    proxy.close()
+    (stats, stats_port) = open_server_socket()
+    stats.close()
+
+    """
+    Create the nutcracker configurations out of the proxy and server ports.
+    """
+
+    srv_A_cfg = simple_nutcracker_config(proxy_port, srv_A_port, cfg_yml_params)
+    srv_B_cfg = simple_nutcracker_config(proxy_port, srv_B_port, cfg_yml_params)
+
+    print("Prepared proxy port %d and stats port %d" % (proxy_port, stats_port))
+
+    ncfg = create_nutcracker_config_file()
+
+    print("Nutcracker config is in %s" % ncfg.name)
+
+    enact_nutcracker_config(ncfg, srv_A_cfg);
+
+    print("Opening nutcracker with config:\n%s" % srv_A_cfg);
+
+    nut_proc = subprocess.Popen([nutcracker_exe, "-c", ncfg.name,
+                                 "-a", "127.0.0.1", "-s", "%d" % stats_port])
+    time.sleep(0.1)
+    nut_proc.poll()
+    if nut_proc.returncode is not None:
+        print("Could not start the nutcracker process: %s\n" % nut_proc.returncode);
+        exit(1)
+
+
+    # Send the request to the proxy. It is supposed to be captured by our
+    # server socket, so check it immediately afterwards.
+
+    client = tcp_connect(proxy_port)
+    client.send("get KEY_FOR_A\r\n")
+
+    print "Accepting connection from proxy..."
+
+    (srv_A, _) = listen_A.accept()
+    data = srv_A.recv(128);
+    if data == "get KEY_FOR_A \r\n":
+        print "Properly received get request by server A"
+        srv_A.send("END\r\n")
+    else:
+        print("Expectation failed: received data: \"%s\"" % data)
+        exit(1)
+
+    print("Now, reloading the nutcracker with config:\n%s" % srv_B_cfg);
+
+    enact_nutcracker_config(ncfg, srv_B_cfg)
+    # Send the "config reload" signal.
+    nut_proc.send_signal(signal.SIGUSR1)
+    time.sleep(0.3)
+
+    client.send("get KEY_FOR_B\r\n")
+
+    (srv_B, _) = listen_B.accept()
+    data = srv_B.recv(128);
+    if data == "get KEY_FOR_B \r\n":
+        print "Properly received get request by server B"
+        srv_B.send("END\r\n")
+    else:
+        print("Expectation failed: received data: \"%s\"" % data)
+        exit(1)
+
+
+
+    nut_proc.kill()
+    nut_proc.wait()
+    if nut_proc.returncode is None:
+        print("Could not stop the nutcracker process\n");
+        exit(1)
+
+    srv_A.close()
+    srv_B.close()
+
+    listen_A.close()
+    listen_B.close()
+
+    print "Finished testing %s" % cfg_yml_params
+
+
 """
-Open two server side sockets.
-One is is for configuration A, another is for configuration B.
-We'll create a nutcracker configuration and make it reload from A to B.
+Testing that reload works across many parameters.
 """
 
-(listen_A, srv_A_port) = open_server_socket()
-(listen_B, srv_B_port) = open_server_socket()
+variants_explored = []
 
-print("Opened two servers on ports %d and %d" % (srv_A_port, srv_B_port))
+for pc in [False, True]:
+  for aeh in [False, True]:
+    for srt in [None, 2000]:
+      for sfl in [1, 2]:
+        cfg_yml_params = {'preconnect': pc,
+                          'auto_eject_hosts': aeh,
+                          'server_retry_timeout': srt,
+                          'server_failure_limit': sfl}
+        test_code_reload(cfg_yml_params)
+        variants_explored.append(cfg_yml_params)
 
-
-"""
-Open the third server on a port which we'll later assign to be a proxy port
-for nutcracker. This is a little bit dangerous due to potential race condition
-with other servers on the local system, but we can't do much better than
-that within the time and space constraints provided for testing. Just don't
-run high load servers or clients on this development machine which does
-integration testing.
-"""
-
-(proxy, proxy_port) = open_server_socket()
-proxy.close()
-(stats, stats_port) = open_server_socket()
-stats.close()
-
-"""
-Create the nutcracker configurations out of the proxy and server ports.
-"""
-
-srv_A_cfg = simple_nutcracker_config(proxy_port, srv_A_port)
-srv_B_cfg = simple_nutcracker_config(proxy_port, srv_B_port)
-
-print("Prepared proxy port %d and stats port %d" % (proxy_port, stats_port))
-
-ncfg = create_nutcracker_config_file()
-
-print("Nutcracker config is in %s" % ncfg.name)
-
-enact_nutcracker_config(ncfg, srv_A_cfg);
-
-print("Opening nutcracker with config:\n%s" % srv_A_cfg);
-
-nut_proc = subprocess.Popen([nutcracker_exe, "-c", ncfg.name,
-                             "-a", "127.0.0.1", "-s", "%d" % stats_port])
-time.sleep(0.1)
-nut_proc.poll()
-if nut_proc.returncode is not None:
-    print("Could not start the nutcracker process: %s\n" % nut_proc.returncode);
-    exit(1)
-
-
-# Send the request to the proxy. It is supposed to be captured by our
-# server socket, so check it immediately afterwards.
-
-client = tcp_connect(proxy_port)
-client.send("get KEY_FOR_A\r\n")
-
-print "Accepting connection from proxy..."
-
-(srv_A, _) = listen_A.accept()
-data = srv_A.recv(128);
-if data == "get KEY_FOR_A \r\n":
-    print "Properly received get request by server A"
-    srv_A.send("END\r\n")
-else:
-    print("Expectation failed: received data: \"%s\"" % data)
-    exit(1)
-
-print("Now, reloading the nutcracker with config:\n%s" % srv_B_cfg);
-
-enact_nutcracker_config(ncfg, srv_B_cfg)
-# Send the "config reload" signal.
-nut_proc.send_signal(signal.SIGUSR1)
-time.sleep(0.1)
-
-client.send("get KEY_FOR_B\r\n")
-
-(srv_B, _) = listen_B.accept()
-data = srv_B.recv(128);
-if data == "get KEY_FOR_B \r\n":
-    print "Properly received get request by server B"
-    srv_B.send("END\r\n")
-else:
-    print("Expectation failed: received data: \"%s\"" % data)
-    exit(1)
-
-
-
-nut_proc.kill()
-nut_proc.wait()
-if nut_proc.returncode is None:
-    print("Could not stop the nutcracker process\n");
-    exit(1)
-
-print "Finished"
-
+print("%d nutcracker configuration variants successfully explored:\n%s"
+        % (len(variants_explored), variants_explored))

08dec2ff009532f562af03c657b19a4a57d77b9f
lwalkin <lwalkin@machinezone.com>
2015-02-04 19:13:35 -0800

    make sure we switch to the library code

--- tests/check_config_reload.py
+++ tests/check_config_reload.py
@@ -12,55 +12,6 @@ except:
     print("This program is designed to run `make check` to test.")
     exit(1)
 
-
-"""
-Teach python to open a socket on some first available local port.
-Return the socket object and the local port obtained.
-"""
-
-def open_server_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("127.0.0.1", 0))
-    s.listen(1)
-    s.settimeout(3.0)
-    (_, port) = s.getsockname()
-    return (s, port)
-
-"""
-Open a client socket and connect to the given port on local machine.
-"""
-
-def tcp_connect(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", port))
-    return s
-
-"""
-Create a simple yaml config with a given port as a server.
-"""
-
-def simple_nutcracker_config(proxy_port, server_port):
-    return ("alpha:\n"
-            + "  listen: 127.0.0.1:%s\n" % proxy_port
-            + "  hash: fnv1a_64\n"
-            + "  distribution: ketama\n"
-            + "  auto_eject_hosts: true\n"
-            + "  server_retry_timeout: 2\n"
-            + "  server_failure_limit: 1\n"
-            #+ "  preconnect: true\n"
-            + "  servers:\n"
-            + "   - 127.0.0.1:%s:1 my_test_server\n" % server_port)
-
-"""
-Make a temporary file which will be used as a configuration for
-nutcracker.
-"""
-
-def nutcracker_config_file():
-    f = tempfile.NamedTemporaryFile(prefix = "nutcracker-", suffix = ".yml")
-    return f
-
-
 """
 Open two server side sockets.
 One is is for configuration A, another is for configuration B.

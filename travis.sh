ecb21543e2f4251a20907b08790f5501f98d718f
idning <idning@gmail.com>
2015-04-24 08:55:04 +0800

    update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,12 +3,14 @@
 #author : ning
 #date   : 2014-05-10 16:54:43
 
-#install
-#sudo pip install redis
-#sudo pip install nose
+#install deps if we are in travis
+if [ -n "$TRAVIS" ]; then
+    sudo pip install redis
+    sudo pip install nose
 
-#pip install git+https://github.com/andymccurdy/redis-py.git@2.9.0
-#pip install git+https://github.com/idning/python-memcached.git#egg=memcache
+    pip install git+https://github.com/andymccurdy/redis-py.git@2.9.0
+    pip install git+https://github.com/idning/python-memcached.git#egg=memcache
+fi 
 
 #build twemproxy
 CFLAGS="-ggdb3 -O0" autoreconf -fvi && ./configure --enable-debug=log && make 

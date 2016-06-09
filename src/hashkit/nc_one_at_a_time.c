b23eb16dcbfe80b87d0a0b8cc98f82e83d0d3baf
Will Fitch <wfitch@meetme.com>
2016-06-09 09:52:48 -0400

    Removed unnecessary key check

--- src/hashkit/nc_one_at_a_time.c
+++ src/hashkit/nc_one_at_a_time.c
@@ -37,12 +37,6 @@ hash_one_at_a_time(const char *key, size_t key_length)
     const char *ptr = key;
     uint32_t value = 0;
 
-    if (key == NULL) {
-        return 0;
-    }
-
-    //log_error("key is %s", key);
-
     while (key_length--) {
         uint32_t val = (uint32_t) *ptr++;
         value += val;
